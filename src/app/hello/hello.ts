fbb022ebf32efc43dd3ed4b65f8611c8ab658118
pitercoding <piterg.bio@gmail.com>
2026-04-29 16:35:30 +0200

    feat(angular): create first standalone component and explore core concepts
    - implement initial standalone Angular component
    - understand modern component structure and setup
    - configure and run tests to verify component behavior
    - use component selector within the application
    - establish foundational knowledge for upcoming features

---
+++ src/app/hello/hello.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  imports: [],
+  templateUrl: './hello.html',
+  styleUrl: './hello.scss',
+})
+export class Hello {}
