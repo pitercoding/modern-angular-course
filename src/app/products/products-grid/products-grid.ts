6e5db8a9c625bac1af004483eed3b0cb6d957b86
pitercoding <piterg.bio@gmail.com>
2026-05-01 11:35:08 +0200

    feat(angular): build products grid and reusable product card UI
    - organize components by feature using a structured folder approach
    
    - implement products grid layout with CSS Grid
    
    - create reusable product card using Angular Material
    
    - apply separation of concerns in component design
    
    - demonstrate standalone component imports in practice
    
    - follow outside-in development by building UI first

---
+++ src/app/products/products-grid/products-grid.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ProductCard } from '../product-card/product-card';
+
+@Component({
+  selector: 'app-products-grid',
+  imports: [ProductCard],
+  templateUrl: './products-grid.html',
+  styleUrl: './products-grid.scss',
+})
+export class ProductsGrid {}
